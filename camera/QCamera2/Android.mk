475a90dd2bee6049d9c91f31b01c0e1e6268b778
Rajaram Gudivada <rajaramg@codeaurora.org>
2016-03-23 20:07:03 +0530

    QCamera2: HAL: FlashLight widget fix for HAL1
    Issue:
    Flashlight in system tray is not working with HAL1 on
    devices with module version >= 2.4.
    Its due to framework looks for ANDROID_FLASH_INFO_AVAILABLE
    property which is not set in the HAL1 camera info.
    
    Fix:
    Populate the required camera static characteristics
    regardless of HAL version.
    
    CRs-Fixed: 993695
    Change-Id: I5d3b95b3b3f4ab72ae3b67c87a8a2d257260a5de

--- camera/QCamera2/Android.mk
+++ camera/QCamera2/Android.mk
@@ -60,6 +60,7 @@ LOCAL_C_INCLUDES := \
         $(LOCAL_PATH)/stack/common \
         $(LOCAL_PATH)/stack/mm-camera-interface/inc \
         $(LOCAL_PATH)/util \
+        $(LOCAL_PATH)/HAL3 \
         hardware/libhardware/include/hardware \
         $(call project-path-for,qcom-media)/libstagefrighthw \
         $(call project-path-for,qcom-media)/mm-core/inc \
